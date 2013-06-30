48fb8bce3325241fefe35902aab482452137c278
Beau Charman <beau.charman@gmail.com>
2013-06-30 23:45:52 +1000

    Comment changes

--- library/javascripts/plugins.js
+++ library/javascripts/plugins.js
@@ -2,11 +2,11 @@
  * Plugins
  * ========================================================================
  * plugins.js
- * @version 2.1 | April 27th 2013
- * @package HTML Project Kickstarter
- * @author  Beau Charman | @beaucharman | http://www.beaucharman.me
- * @link    https://github.com/beaucharman/HTML-Project-Kickstarter
- * @license MIT License
+ * @version   2.1 | April 27th 2013
+ * @package   HTML Project Kickstarter
+ * @author    Beau Charman | @beaucharman | http://www.beaucharman.me
+ * @link      https://github.com/beaucharman/HTML-Project-Kickstarter
+ * @license   MIT License
  *
  * Thank you http://html5boilerplate.com/
  * ======================================================================== */
@@ -18,24 +18,25 @@
 var documentClassName = document.documentElement.className;
 documentClassName = documentClassName.replace(/\bno-js\b/g, '') + ' js ';
 
-/* Avoid `console` errors in browsers that lack a console. [from boilerplate] */
+/**
+ * Avoid `console` errors in browsers that lack a console. [from boilerplate]
+ */
 (function () {
-    var method;
-    var noop = function () {};
-    var methods = [
-        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
-        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
-        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
-        'timeStamp', 'trace', 'warn'
-    ];
-    var length = methods.length;
-    var console = (window.console = window.console || {});
-
-    while (length--) {
-        method = methods[length];
-        // Only stub undefined methods.
-        if (!console[method]) {
-            console[method] = noop;
-        }
+  'use strict';
+  var method;
+  var noop = function () {};
+  var methods = [
+    'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+    'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+    'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+    'timeStamp', 'trace', 'warn'
+  ];
+  var length = methods.length;
+  var console = (window.console = window.console || {});
+  while (length--) {
+    method = methods[ length ];
+    if (! console[method]) {
+      console[ method ] = noop;
     }
+  }
 }());
