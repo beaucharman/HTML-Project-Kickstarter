f35e4f3156d9587cf0f5fd92de497af0cfcb917f
Beau Charman <beau.charman@gmail.com>
2013-08-25 17:11:01 +1000

    refresh

--- library/javascripts/main.js
+++ library/javascripts/main.js
@@ -2,20 +2,17 @@
  * Main
  * ========================================================================
  * main.js
- * @version 2.1 | April 27th 2013
- * @package HTML Project Kickstarter
- * @author  Beau Charman | @beaucharman | http://www.beaucharman.me
- * @link    https://github.com/beaucharman/HTML-Project-Kickstarter
- * @license MIT License
- *
- * Thank you http://html5boilerplate.com/
- * ======================================================================== */
+ * @version   2.1 | April 27th 2013
+ * @package   HTML Project Kickstarter
+ * @author    Beau Charman | @beaucharman | http://www.beaucharman.me
+ * @link      https://github.com/beaucharman/HTML-Project-Kickstarter
+ * @license   MIT License
+ */
 
-// jQuery No Conflict
-// To prevent thrid party plugins from conflicting with
-// your jQuery, simple use the following anonymous function:
+// jQuery No Conflict. To prevent third party plugins from conflicting with
+// your jQuery, simply use the following anonymous function:
 /*
-(function($) {
-  // ...
-}(jQuery));
+;(function ($) {
+  // contain code in here...
+})(jQuery);
 */
